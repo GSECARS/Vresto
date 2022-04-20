a5a58cca39ff2df8b3cc543f3dcfb808cab98f8b
skordaschristofanis <skordas.chr@hotmail.com>
2022-04-20 11:13:39 -0500

    Added QLine

--- vresto/widget/custom/q_line.py
+++ vresto/widget/custom/q_line.py
@@ -18,5 +18,22 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------
 
-from vresto.widget.custom.msg_box import MsgBox
-from vresto.widget.custom.q_line import QLine
+from qtpy.QtWidgets import QFrame
+from typing import Optional
+
+
+class QLine(QFrame):
+    """Custom vertical/horizontal line widget to expand to the available space."""
+
+    def __init__(self, vertical: Optional[bool] = True) -> None:
+        super(QLine, self).__init__()
+
+        self._vertical = vertical
+        self._set_orientation()
+
+    def _set_orientation(self) -> None:
+        """Sets the orientation of the line to vertical or horizontal."""
+        if self._vertical:
+            self.setFrameShape(QFrame.Shape.VLine)
+        else:
+            self.setFrameShape(QFrame.Shape.HLine)

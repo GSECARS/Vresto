933be826e88bc782a5cbb59cd1ea6c4617e51483
skordaschristofanis <skordas.chr@hotmail.com>
2022-04-20 13:18:42 -0500

    Reformatted controller

--- vresto/controller/main_controller.py
+++ vresto/controller/main_controller.py
@@ -28,6 +28,8 @@ from vresto.model import MainModel, QtWorkerModel
 
 
 class MainController(QObject):
+    """Base controller, initializes sub-controllers, creates and run main app worker and checks for epics connection."""
+
     _epics_connection_changed: Signal = Signal(bool)
 
     def __init__(self) -> None:
@@ -40,36 +42,46 @@ class MainController(QObject):
         # Event helpers
         self._time_started = None
 
+        # Connect epics connection signal
         self._epics_connection_changed.connect(self._update_epics_status_label)
 
+        # Application thread worker
         self._main_worker = QtWorkerModel(self._worker_methods, ())
         self._main_worker.start()
 
     def run(self, version: str) -> None:
+        """Starts the application."""
         self._widget.display(version=version)
         sys.exit(self._app.exec())
 
     def _update_epics_status_label(self, status: bool) -> None:
+        """Updates the circle status label based on epics connection."""
         self._widget.lbl_epics_status.setEnabled(status)
 
     def _check_epics_connection(self) -> None:
         """Checks the epics connection every 5 minutes."""
-        #
+        # Initialize first connection if needed
         if self._time_started is None:
             self._time_started = time.time()
             self._model.epics.connect()
+            self._epics_connection_changed.emit(self._model.epics.connected)
 
         now = time.time()
+        # If time difference is more than 5 minutes try epics connect again
         if now - self._time_started > 300:
             self._time_started = time.time()
 
             self._model.epics.connect()
 
-        self._epics_connection_changed.emit(self._model.epics.connected)
+            # Emit connection changed signal
+            self._epics_connection_changed.emit(self._model.epics.connected)
 
     def _worker_methods(self) -> None:
+        """Runs all the worker methods."""
+
         while not self._widget.terminated:
             self._check_epics_connection()
             time.sleep(0.05)
 
+        # Set as finished so UI can exit
         self._widget.worker_finished = True

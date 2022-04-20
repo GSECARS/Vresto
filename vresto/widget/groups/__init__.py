759fff39a73b8dbafb919dc866b555d99530bbfa
skordaschristofanis <skordas.chr@hotmail.com>
2022-04-20 14:04:14 -0500

    Added groups

---
+++ vresto/widget/groups/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# ----------------------------------------------------------------------
+# vresto - Diamond Anvil Cell Corrections GUI software.
+# Author: Christofanis Skordas (skordasc@uchicago.edu)
+# Copyright (C) 2022  GSECARS, The University of Chicago
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------
+
+from vresto.widget.groups.pinhole_group import PinholeGroup
+from vresto.widget.groups.microscope_group import MicroscopeGroup
+from vresto.widget.groups.corrections_group import CorrectionsGroup
+from vresto.widget.groups.common_controls_group import CommonControlsGroup
+from vresto.widget.groups.diamond_images_group import DiamondImagesGroup
+from vresto.widget.groups.mirror_group import MirrorGroup
+from vresto.widget.groups.sample_group import SampleGroup

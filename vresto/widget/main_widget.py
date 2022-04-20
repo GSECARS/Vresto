5574228a094a09a0ac1c9a90976bef6e2967be48
skordaschristofanis <skordas.chr@hotmail.com>
2022-04-20 11:09:14 -0500

    Added epics and hutch status widgets.

--- vresto/widget/main_widget.py
+++ vresto/widget/main_widget.py
@@ -18,25 +18,125 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------
 
-from qtpy.QtWidgets import QMainWindow, QTabWidget
+import os
+from qtpy.QtWidgets import QMainWindow, QTabWidget, QMessageBox, QLabel, QFrame, QGridLayout, QHBoxLayout, QVBoxLayout
+from qtpy.QtCore import QSize
+from qtpy.QtGui import QIcon, QCloseEvent
+
+from vresto.model import PathModel
 
 
 class MainWidget(QMainWindow):
     """The main application widget."""
 
-    _title = f"Vresto"
+    _size: QSize = QSize(780, 860)
+    _hutch: str = ""
 
-    def __init__(self) -> None:
+    def __init__(self, paths: PathModel) -> None:
         super(MainWidget, self).__init__()
 
+        self._paths = paths
+
+        # Groups - Define groups below
+
+        self._main_frame = QFrame()
         self._tab_widget = QTabWidget()
+        self.alignment_widget = None
+        self.lbl_epics_status = QLabel()
+        self._lbl_hutch = QLabel(self._hutch)
 
         # Enable the status bar
         self.statusBar()
 
-    def display(self, version: str) -> None:
+        # Event helpers
+        self._terminated: bool = False
+        self._worker_finished: bool = False
 
+        self._configure_tab_widget()
+        self._configure_epics_status_widgets()
+        self._configure_main_frame()
+        self._configure_widget()
+
+    def _configure_widget(self) -> None:
+        """Basic window configuration."""
+        # Set the object name
+        self.setObjectName("QMainWindow")
+
+        # Set the icon
+        self.setWindowIcon(QIcon(os.path.join(self._paths.icon_path, "diamond.png")))
+
+        # Load qss
+        self.setStyleSheet(open(os.path.join(self._paths.qss_path, "main.qss"), "r").read())
+
+        # Set maximum size
+        self.setMaximumSize(self._size)
+
+        # Set central widget
+        self.setCentralWidget(self._main_frame)
+
+    def _configure_tab_widget(self) -> None:
+        """Configures the main tab widget (central widget)."""
+        # Add tabs
+        self._tab_widget.addTab(self.alignment_widget, "ALIGNMENT")
+
+    def _configure_epics_status_widgets(self) -> None:
+        # Circle indicator
+        self.lbl_epics_status.setEnabled(False)
+        self.lbl_epics_status.setObjectName("lbl-epics")
+
+        # Hutch label
+        self._lbl_hutch.setObjectName("lbl-hutch")
+
+    def _configure_main_frame(self) -> None:
+
+        horiz_layout = QHBoxLayout()
+        horiz_layout.setSpacing(0)
+        horiz_layout.addStretch(1)
+        horiz_layout.addWidget(self.lbl_epics_status)
+        horiz_layout.addWidget(self._lbl_hutch)
+
+        vert_layout = QVBoxLayout()
+        vert_layout.addLayout(horiz_layout)
+        vert_layout.addStretch(1)
+
+        layout = QGridLayout()
+        layout.addWidget(self._tab_widget, 0, 0, 1, 1)
+        layout.addLayout(vert_layout, 0, 0, 1, 1)
+
+        self._main_frame.setLayout(layout)
+
+    def display(self, version: str) -> None:
+        """Sets the window title and the window visibility to normal."""
         # Set the title
-        self.setWindowTitle(f"{self._title} {version}")
+        self.setWindowTitle(f"Vresto {version}")
 
         self.showNormal()
+
+    def closeEvent(self, event: QCloseEvent) -> None:
+        """Creates a message box for exit confirmation if closeEvent is triggered."""
+        _msg_question = QMessageBox.question(
+            self, "Exit confirmation", "Are you sure you want to close the application?"
+        )
+
+        if _msg_question == QMessageBox.Yes:
+
+            self._terminated = True
+
+            while not self._worker_finished:
+                continue
+
+            event.accept()
+        else:
+            event.ignore()
+
+    @property
+    def terminated(self) -> bool:
+        return self._terminated
+
+    @property
+    def worker_finished(self) -> bool:
+        return self._worker_finished
+
+    @worker_finished.setter
+    def worker_finished(self, value: bool) -> None:
+        self._worker_finished = value
